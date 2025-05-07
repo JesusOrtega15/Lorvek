4d07aed01a9529695cd074f788a5683c0ddffd02
Max <134027604+JesusOrtega15@users.noreply.github.com>
2025-05-06 22:00:49 -0600

    Update web.py

--- gemini_api/web.py
+++ gemini_api/web.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from chat import sample_question
+from .chat import sample_question
 app = Flask(__name__, template_folder=".")
 
 @app.route('/', methods=['GET', 'POST'])
