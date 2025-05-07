2c7a67cccfd6c56d276e1ab87bd9214e212d4100
marimaxorvalov <marimaxorvalov@gmail.com>
2025-05-07 03:10:04 +0000

    crear archivos

---
+++ gemini_api/chat.py
@@ -0,0 +1,20 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+from google.genai import types
+
+load_dotenv()
+client = genai.Client(api_key = os.environ.get('API_KEY', "not found"))
+config = types.GenerateContentConfig(
+    temperature=1.2,
+    top_k=40,
+    top_p=0.9,
+    max_output_tokens=1024,
+    system_instruction="Eres un inteligencia Artificial Llamada Lorvek AI creada por Maximiliano, no respondas tan seco y metele actitud"
+)
+chat = client.chats.create(model="gemini-2.0-flash", config=config)
+
+
+def sample_question(message):
+	respon= chat.send_message(message)
+	return respon.text
---
+++ gemini_api/web.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from chat import sample_question
+app = Flask(__name__, template_folder=".")
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    response = None
+    if request.method == 'POST':
+
+        query = request.form.get('name')
+        response = sample_question(query)
+
+    return render_template("indice.html", response=response)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
