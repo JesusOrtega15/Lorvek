ad95445533c3ab5abacec07cbe421994cae9ef4c
Max <134027604+JesusOrtega15@users.noreply.github.com>
2025-05-06 23:08:57 -0600

    Update web.py

--- gemini_api/web.py
+++ gemini_api/web.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from .chat import sample_question
+from flask import Flask, render_template, request, jsonify
+from .chat import sample_question, get_history
 app = Flask(__name__, template_folder=".")
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,6 +12,16 @@ def index():
 
     return render_template("indice.html", response=response)
     
+@app.route('history')
+def history():
+    history = get_history()
+    conversacion = []
+    
+    for message in history:
+        role = message.role
+        text = message.parts[0].text
+        conversacion.append({'rol': role, 'texto':text})
 
+    return jsonify(conversacion)
 if __name__ == '__main__':
     app.run(debug=True)
